5e950f85c90b24307e4cfec0af9ec37a5f23f0cb
Captility <deppe.daniel@gmail.com>
2014-08-21 20:09:52 +0200

    Device - Addon Preparations implemented.

--- app/Model/Event.php
+++ app/Model/Event.php
@@ -142,6 +142,16 @@ class Event extends AppModel {
                 //'on' => 'create', // Limit validation to 'create' or 'update' operations
             ),
         ),
+        'device_id' => array(
+            'numeric' => array(
+                'rule' => array('numeric'),
+                //'message' => 'Your custom message here',
+                //'allowEmpty' => false,
+                //'required' => false,
+                //'last' => false, // Stop validation after this rule
+                //'on' => 'create', // Limit validation to 'create' or 'update' operations
+            ),
+        ),
     );
 
     //The Associations below have been created with all possible keys, those that are not needed can be removed
@@ -169,6 +179,13 @@ class Event extends AppModel {
         'Capture' => array(
             'className' => 'Capture',
             'foreignKey' => 'capture_id',
+            'conditions' => '',
+            'fields' => '',
+            'order' => ''
+        ),
+        'Device' => array(
+            'className' => 'Device',
+            'foreignKey' => 'device_id',
             'conditions' => '',
             'fields' => '',
             'order' => ''
